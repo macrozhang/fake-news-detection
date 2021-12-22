6d19903a9f7de468604098b2a87c5502e47acae5
JamesMacroZhang <33259827+macrozhang@users.noreply.github.com>
2021-12-22 22:24:38 +0100

    Update preprocessor.py
    Add function clean_text(text)

---
+++ services/stream/utils/preprocessor.py
@@ -0,0 +1,34 @@
+import re
+import pandas as pd
+
+from utils.contractions import contractions
+
+
+def clean_text(text):
+    '''Remove unwanted characters, stopwords, and format the text to create fewer nulls word embeddings'''
+    
+    # Convert words to lower case
+    text = text.lower()
+    
+    # Replace contractions with their longer forms 
+    if True:
+        text = text.split()
+        new_text = []
+        for word in text:
+            if word in contractions:
+                new_text.append(contractions[word])
+            else:
+                new_text.append(word)
+        text = " ".join(new_text)
+    
+    # Format words and remove unwanted characters
+    text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+    text = re.sub(r'\<a href', ' ', text)
+    text = re.sub(r'&amp;', '', text) 
+    text = re.sub(r'[_"\-;%()|+&=*%.,!?:#$@\[\]/]', ' ', text)
+    text = re.sub(r'<br />', ' ', text)
+    text = re.sub(r'\'', ' ', text)
+    
+    return text
+
+
