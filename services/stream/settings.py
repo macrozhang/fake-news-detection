75cb43551e796f68c5b151f848a17f3e125affc5
Zolotareva Ekaterina <zolotarevaekaterina567@gmail.com>
2021-12-23 02:04:34 +0100

    insert data from the string to the db

--- services/stream/settings.py
+++ services/stream/settings.py
@@ -6,3 +6,4 @@ BASE_DIR = Path(__file__).resolve().parent
 # dotenv.load_dotenv(f'{BASE_DIR}/.env')
 
 BATCH_SIZE = int(os.environ.get('BATCH_SIZE'))
+DB_CONNECTION = str(os.environ.get('DB_CONNECTION'))
--- services/stream/stream.py
+++ services/stream/stream.py
@@ -1,16 +1,32 @@
 import sys
 import os
+import time
+import uuid
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
+from catboost import CatBoostClassifier
 
 from utils import preprocessor
+from utils.db_utils import CassandraDb
+
+from settings import DB_CONNECTION
 
 """
 This is use for create streaming of text from txt files that creating dynamically 
 from files.py code. This spark streaming will execute in each 1 seconds and It'll
 show number of words count from each files dynamically
 """
+
+print("DB is warming up...")
+time.sleep(10)
+db = CassandraDb([DB_CONNECTION], "9042")
+
+keyspaceName = "fakenews"
+tableName = "dataset"
+
+db.create_keyspace(keyspaceName)
+db.create_table(keyspaceName, tableName, {"id" : "uuid PRIMARY KEY","title": "text", "category" : "text", "prediction": "text"})
 
 def readMyStream(rdd, spark):
   if not rdd.isEmpty():
@@ -21,16 +37,29 @@ def readMyStream(rdd, spark):
 
     df = df.select('id','news_url','title','tweet_ids', 'Y', 'category' )
     df.show()
-    
+
     cleaned_df = preprocessor.clean_df(df)
     cleaned_df.head()
 
     clm = CatBoostClassifier()
     clm.load_model("models/weights/catboost", format='cbm')
 
-    predictions = clf.predict(data=cleaned_df)
+    predictions = clm.predict(data=cleaned_df)
 
     cleaned_df['Y'] = predictions
+    print(f"predictions : {predictions}")
+
+    predicted_data = []
+    for index, row in cleaned_df.iterrows():
+        # print(row['c1'], row['c2'])
+        predicted_data.append({
+			"id" : uuid.uuid4(),
+			"title": row['clean_title'], 
+			"category": row['category'],
+			"prediction": row['Y']})
+
+    db.insert_batch_data(predicted_data, keyspaceName, tableName)
+
 
 
 
